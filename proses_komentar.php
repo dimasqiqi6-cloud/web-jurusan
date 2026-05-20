4cace435aa0277de0f72ea3f3d3a575d2c5940e0
dimasqiqi6-cloud <dimasqiqi6@gmail.com>
2026-05-20 14:44:09 +0700

    proses_komentar.php

--- proses_komentar.php
+++ proses_komentar.php
@@ -5,7 +5,7 @@ $nama = $_POST['nama'];
 $pesan = $_POST['pesan'];
 $parent_id = !empty($_POST['parent_id']) ? $_POST['parent_id'] : NULL;
 
-// CEK RATING (HANYA UNTUK KOMENTAR UTAMA)
+
 if ($parent_id === NULL) {
     if (empty($_POST['rating'])) {
         echo "Harus kasih rating dulu!";
@@ -13,11 +13,11 @@ if ($parent_id === NULL) {
     }
     $rating = $_POST['rating'];
 } else {
-    // BALASAN TIDAK PAKAI RATING
+  
     $rating = 0;
 }
 
-// QUERY INSERT
+
 if ($parent_id === NULL) {
     $query = "INSERT INTO komentar (nama, pesan, rating, parent_id)
               VALUES ('$nama', '$pesan', '$rating', NULL)";
@@ -26,7 +26,6 @@ if ($parent_id === NULL) {
               VALUES ('$nama', '$pesan', '$rating', '$parent_id')";
 }
 
-// EKSEKUSI
 if (!mysqli_query($conn, $query)) {
     die("Error: " . mysqli_error($conn));
 }
